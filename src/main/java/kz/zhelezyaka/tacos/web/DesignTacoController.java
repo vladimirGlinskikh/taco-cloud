b7b4166eb1cee0663991ea540caae2911a01ba5f
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-22 00:24:23 +0600

    Validation errors displayed on the order form

--- src/main/java/kz/zhelezyaka/tacos/web/DesignTacoController.java
+++ src/main/java/kz/zhelezyaka/tacos/web/DesignTacoController.java
@@ -50,7 +50,10 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors) {
+        if (errors.hasErrors()){
+            return "design";
+        }
         log.info("Processing design: " + design);
         return "redirect:/orders/current";
     }
